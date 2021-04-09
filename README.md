b75e4f619583af0e4e9db80ef75a864bbdafc46d
Pavel Zaitsev <mefmor@gmail.com>
2021-04-09 20:59:20 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#Spring Boot SOAP Web Service
+
+
+https://docs.spring.io/spring-ws/docs/3.0.10.RELEASE/reference/
+
+Этот пример создан по образу и подобию:
+https://spring.io/guides/gs/producing-web-service/
+но имеет следующие отличия:
+- Генерация классов происходит на основе существующего WSDL, а не как в примере выше - из XSD
+- Сервис создаётся на основе существующей WSDL
+
+Эндпоинт после запуска: http://localhost:8080/ws/  
+Получить WSDL: http://localhost:8080/ws/countries.wsdl  
---
+++ src/main/java/net/mefmor/sample/hr/WebServiceConfig.java
@@ -0,0 +1,30 @@
+package net.mefmor.sample.hr;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "countries")
+    public SimpleWsdl11Definition simpleWsdl11Definition() {
+        return new SimpleWsdl11Definition(new ClassPathResource("countries.wsdl"));
+    }
+
+}
